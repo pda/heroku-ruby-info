0037e871f14845c763a182163272401f6a2ade13
Paul Annesley <paul@annesley.cc>
2013-03-01 09:28:11 +1100

    Show ruby, gem, bundler versions, and Gemfile.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require "sinatra"
 
+COMMANDS = [
+  "ruby --version",
+  "gem --version",
+  "bundle --version",
+]
+
 get "/" do
+
+  @commands = Hash[
+    COMMANDS.map { |cmd| [cmd, `#{cmd}`] }
+  ]
+
+  @gemfile = File.read("Gemfile")
+
+  erb :index
+
 end
